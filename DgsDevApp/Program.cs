9ed588b66af5c24b1be6911af841fb8c9eec5bbb
Douglas Pinheiro <dgspramos@gmail.com>
2021-06-19 16:01:04 -0300

    Adicionar arquivos de projeto.

---
+++ DgsDevApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace DgsDevApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.35865;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+
+            Console.WriteLine(x.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine(" \n DGSDEV 2021\n");
+
+            Console.WriteLine(" Hello World!\n");
+
+            Console.WriteLine(" Aprendendo: C#, .NET 5, Api Rest com Asp.NET Core 5, Entity FrameWork Core 5 e Blazor\n");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(" " + i + "  ----> DGSDEV ITAÚ UNIBANCO 2021 \r");
+
+                Console.ReadLine();
+
+            }
+
+            Console.WriteLine(" Bye Bye! ;)");
+            Console.ReadLine();
+        }
+    }
+}
